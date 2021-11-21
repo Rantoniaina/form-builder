9c6a41a7c51822336c39184308ee789880a5ae54
Andriamampionona Jaona Rantoniaina <jaona.rantoniaina@gmail.com>
2021-11-21 17:10:41 +0300

    :zap: Delete unused code and configure tailwindss

---
+++ client/tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  purge: ['./pages/**/*.{jts,tsx}', './components/**/*.{ts,tsx}'],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
